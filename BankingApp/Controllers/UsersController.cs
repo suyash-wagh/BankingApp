362c827732b41bec8899cc712e515b0f27489a22
Ujjay-99 <ujjay99@gmail.com>
2022-05-21 16:14:47 +0530

    added download-csv passbook

--- BankingApp/Controllers/UsersController.cs
+++ BankingApp/Controllers/UsersController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -93,6 +94,25 @@ namespace BankingApp.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+        public void CsvDownload()
+        {
+            User user = Session["User"] as User;
+
+            StringWriter sw = new StringWriter();
+            sw.WriteLine("\"Name\",\"Date\",\"Amount\",\"Transaction Type\"");
+            Response.ClearContent();
+            Response.AddHeader("content-disposition", "attchment;filename=${user} Passbook.csv");
+            Response.ContentType=("text/csv");
+            List<Transaction> model = db.Transactions.Where(u => u.Name == user.Name).ToList();
+            foreach (var item in model)
+            {
+                sw.WriteLine(string.Format("\"{0}\",\"{1}\",\"{2}\",\"{3}\"", item.Name, item.Created, item.Amount, item.TransactionType));
+
+            }
+            Response.Write(sw.ToString());
+            Response.End();
+
         }
     }
 }
